613fe0fd3526bc346395590b8f19b96be514b57f
Michal Kapusnik <michal.kapusnik@gmail.com>
2019-01-29 00:36:00 +0100

    Initial balena configuration

--- gpio.py
+++ gpio.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
-#import logging
-
-#logging.basicConfig(format='%(levelname)s-%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG,filename='/App/gpio.log')
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(7, GPIO.OUT)
 
 while True:
-  GPIO.output(7, 1)
-  time.sleep(1)
-  GPIO.output(7, 0)
-  time.sleep(1)	
+    GPIO.output(7, 1)
+    time.sleep(1)
+    GPIO.output(7, 0)
+    time.sleep(1)	
 
 GPIO.cleanup()
